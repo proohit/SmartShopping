9c28d01a5aa941865b0729d2c7a5b4c9e399b7b4
Direnc Timur <direnc99@gmail.com>
2020-12-01 12:02:14 +0100

    add cors

--- server.py
+++ server.py
@@ -4,12 +4,13 @@ from resources.ProductResource import ProductResource
 from flask import Flask
 from flask_restful import Api
 from repositories.DbManager import dbmanager
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
-
-dbmanager.reset_database()
-dbmanager.initialize_database()
+# dbmanager.reset_database()
+# dbmanager.initialize_database()
 
 api.add_resource(ProductResource, '/products', endpoint='products')
 api.add_resource(RecommendationResource,
