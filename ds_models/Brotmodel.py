4ccf517003efdb85f095626ac3fa233e68083461
bk094 <bk094@hdm-stuttgart.de>
2020-12-08 19:21:53 +0100

    Problem mit 'flask.wrappers.Response' to JSON

--- ds_models/Brotmodel.py
+++ ds_models/Brotmodel.py
@@ -1,23 +1,54 @@
 import pickle
-# import jsonpickle
+import jsonpickle
 from resources.CustomerResource import CustomerResource
-# from flask import make_response
-# from flask_restful import Resource
+from repositories.DbManager import dbmanager
+from repositories.CustomerRepository import CustomerRepository
+from flask import make_response
+from flask_restful import Resource
 from flask.views import MethodView
-#import pandas as pd
+import pandas as pd
 import xgboost
 import json
 
 ## Daten empfangen:
-customer = CustomerResource
-print(customer)
-loaded_model = pickle.load(open("/Users/benjaminkampka/Desktop/Studium/Semester 7/Future Retail/SmartShopping/ds_models/pima.picklebrot.txt", "rb"))
+#customers = CustomerResource
+loaded_model = pickle.load(
+    open("/Users/benjaminkampka/Desktop/Studium/Semester 7/Future Retail/SmartShopping/ds_models/pima.picklebrot.txt",
+         "rb"))
+
 
 ## Prediction
-class test(MethodView):
+# class test(MethodView):
+#   def get(self):
+#pred = loaded_model.predict(customers)
+
+
+class BrotPrediction(Resource):
     def get(self):
-        pred = loaded_model.predict(customer)
-        return pred
+        customer_repo = CustomerRepository(dbmanager)
+        customers = customer_repo.get_all_customer()
+        response_customer = make_response(
+           jsonpickle.encode(customers, unpicklable=False))
+        response_customer.headers['content-type'] = 'application/json'
+        #print(response_customer)
+        #test = json.dumps(response_customer)
+        #test = pd.DataFrame(response_customer,index=response_customer[:,0])
+        #print(test)
+        #pred = loaded_model.predict(response_customer)
+        #print(pred)
+
+        # HIER SCHAUEN:
+        # -> https://stackoverflow.com/questions/25398218/getting-json-response-using-requests-object-flask
+        print(type(response_customer))
+        return response_customer
+
+
+       # print(customer)
+       # response = make_response(jsonpickle.encode(customer,
+                                                  # unpicklable=False))  # {"py/type": "resources.CustomerResource.CustomerResource"}
+       # response.headers[
+        #    'content-type'] = 'application/json'  # {"py/type": "resources.CustomerResource.CustomerResource"}
+      #  return response
 
 ## Bsp. Ausgabe
 # print(pred)  # Zusätzliche SQL Abfrage der Productdatenbank <- eventuell hier Sortierung
--- server.py
+++ server.py
@@ -2,11 +2,11 @@
 from resources.RecommendationResource import RecommendationResource
 from resources.ProductResource import ProductResource
 from resources.CustomerResource import CustomerResource
-from ds_models.Brotmodel import test
+from ds_models.Brotmodel import BrotPrediction
 from flask import Flask
 from flask_restful import Api
 #from flask.views import MethodView
-# from repositories.DbManager import dbmanager
+#from repositories.DbManager import dbmanager
 from flask_cors import CORS
 import xgboost
 
@@ -20,8 +20,8 @@ api = Api(app)
 api.add_resource(ProductResource, '/products', endpoint='products')
 api.add_resource(RecommendationResource, '/products/recommendations', endpoint='productRecommendations')
 api.add_resource(CustomerResource, '/customers', endpoint='customers')
-#api.add_resource(test, '/bread', endpoint='bread')
-app.add_url_rule('/bread', view_func=test.as_view('pred'))
+api.add_resource(BrotPrediction, '/bread', endpoint='bread')
+#app.add_url_rule('/bread', view_func=test.as_view('pred'))
 
 
 if __name__ == "__main__":
