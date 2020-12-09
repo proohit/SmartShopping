fa0533c4929cc538937976ddfc74afc2839a4f20
bk094 <bk094@hdm-stuttgart.de>
2020-12-09 15:25:46 +0100

    class BrotPrediction liefert einen String mit der Vorhersage 0,1 o. 2

--- ds_models/Brotmodel.py
+++ ds_models/Brotmodel.py
@@ -1,56 +1,34 @@
 import pickle
-import jsonpickle
-from resources.CustomerResource import CustomerResource
 from repositories.DbManager import dbmanager
 from repositories.CustomerRepository import CustomerRepository
-from flask import make_response
 from flask_restful import Resource
-from flask.views import MethodView
 import pandas as pd
+from json import JSONEncoder
+import numpy
 import xgboost
 import json
 
-## Daten empfangen:
-#customers = CustomerResource
+# customers = CustomerResource
 loaded_model = pickle.load(
     open("/Users/benjaminkampka/Desktop/Studium/Semester 7/Future Retail/SmartShopping/ds_models/pima.picklebrot.txt",
          "rb"))
 
+def obj_dict(obj):
+    return obj.__dict__
 
-## Prediction
-# class test(MethodView):
-#   def get(self):
-#pred = loaded_model.predict(customers)
-
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
 
 class BrotPrediction(Resource):
     def get(self):
         customer_repo = CustomerRepository(dbmanager)
         customers = customer_repo.get_all_customer()
-        response_customer = make_response(
-           jsonpickle.encode(customers, unpicklable=False))
-        response_customer.headers['content-type'] = 'application/json'
-        #print(response_customer)
-        #test = json.dumps(response_customer)
-        #test = pd.DataFrame(response_customer,index=response_customer[:,0])
-        #print(test)
-        #pred = loaded_model.predict(response_customer)
-        #print(pred)
+        customerjson = json.dumps(customers, default=obj_dict)
+        pandadf = pd.read_json(customerjson)
+        pred = loaded_model.predict(pandadf)
+        predictionstring = json.dumps(pred, cls=NumpyArrayEncoder)
+        return predictionstring # liefert endlich ausgabe ->
 
-        # HIER SCHAUEN:
-        # -> https://stackoverflow.com/questions/25398218/getting-json-response-using-requests-object-flask
-        print(type(response_customer))
-        return response_customer
-
-
-       # print(customer)
-       # response = make_response(jsonpickle.encode(customer,
-                                                  # unpicklable=False))  # {"py/type": "resources.CustomerResource.CustomerResource"}
-       # response.headers[
-        #    'content-type'] = 'application/json'  # {"py/type": "resources.CustomerResource.CustomerResource"}
-      #  return response
-
-## Bsp. Ausgabe
-# print(pred)  # Zusätzliche SQL Abfrage der Productdatenbank <- eventuell hier Sortierung
-# GET
-## -> Hier muss die "pred" zurpckgeschickt werden.
